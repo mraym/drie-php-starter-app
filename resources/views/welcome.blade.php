5334f0057c45feefe520ee91ed64e5b8df558729
Kalbir Sohi <kalbir@users.noreply.github.com>
2017-07-21 17:19:08 +0100

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Welcome to Mexico!
                 </div>
 
                 <div class="links">
