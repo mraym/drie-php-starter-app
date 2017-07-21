16fbd6ae24271cafb95ee1d375171060075b71f1
Kalbir Sohi <kalbir@users.noreply.github.com>
2017-07-21 16:54:52 +0100

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Welcome Kalbir
+                    Welcome Kalbir-san
                 </div>
 
                 <div class="links">
